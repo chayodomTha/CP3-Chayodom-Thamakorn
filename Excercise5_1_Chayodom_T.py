58b31d233a544b380dc5603b57e009c98b949984
ChaydomTha <97500068+chayodomTha@users.noreply.github.com>
2022-01-15 14:40:44 +0700

    Excercise5_2

---
+++ Excercise5_1_Chayodom_T.py
@@ -0,0 +1,7 @@
+x = int(input("Input X:"))
+y = int(input("Input Y:"))
+
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "*", y, "=", x * y)
+print(x, "/", y, "=", int(x / y))
---
+++ Excercise5_2_Chayodom_T.py
@@ -0,0 +1,7 @@
+x = int(input("Distance (km) ="))
+y = int(input("Time (hr) ="))
+if x>0 and y>0:
+    print("Velocity =", x/y,"km/hr")
+else:
+    print("Please input data > 0")
+
