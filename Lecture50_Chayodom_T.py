0e8477893eaeda5360230e32882139613d1be2ac
ChaydomTha <97500068+chayodomTha@users.noreply.github.com>
2022-01-16 01:15:16 +0700

    Create Lecture50_Chayodom_T.py

---
+++ Lecture50_Chayodom_T.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def subtractNumber(x,y):
+    print(x-y)
+def divideNumber(x,y):
+    print(x/y)
+def MutiplyNumber(x,y):
+    print(x*y)
+
+addNumber(100,500)
+subtractNumber(100,50)
+divideNumber(100,10)
+MutiplyNumber(2,10)
