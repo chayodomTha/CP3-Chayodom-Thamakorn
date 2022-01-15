3b04b29a68342a44efef048de4eb1dde315be118
ChaydomTha <97500068+chayodomTha@users.noreply.github.com>
2022-01-15 13:40:56 +0700

    Create Excercise4_Chayodom_T.py

---
+++ Excercise4_Chayodom_T.py
@@ -0,0 +1,12 @@
+Eng_Score = 50
+BU_Score = 80
+IntroCom_Score = 89
+ComProgram_Score = 100
+
+print("--- Your Score ---")
+print("Foundation English:",Eng_Score)
+print("General Business:",BU_Score)
+print("Introduction to Computer Systems:",IntroCom_Score)
+print("Computer Programming:",ComProgram_Score)
+
+
