2d502823f84af0b885f526f5a092fabe80cc0f92
ChaydomTha <97500068+chayodomTha@users.noreply.github.com>
2022-01-15 15:41:31 +0700

    Create Excercise8_Chayodom_T.py

---
+++ Excercise8_Chayodom_T.py
@@ -0,0 +1,28 @@
+from ast import Pass
+
+
+User = input("Username =")
+Pass = input("Password =")
+if  User == "chayodom" and Pass == "thamakorn":
+    print("--------------------------")
+    print("Welcome To Chayodom's Shop")
+    print("--------------------------")
+    print("Menu")
+    print("No.1 : Banana (Price 50)")
+    print("No.2 : Cake (Price 70)")
+    print("No.3 : Apple (Price 80)")
+    print("--------------------------")
+    print("Please Select no. of menu")
+    x = int(input("No. ="))
+    if  x==1:
+        NoBanana = int(input("Please input amount of Banana ="))
+        print("Total Price :", 50, "x", NoBanana, "=", 50*NoBanana, "THB")
+    elif  x==2:
+        NoCake = int(input("Please input amount of Cake ="))
+        print("Total Price :", 70, "x", NoCake, "=", 70*NoCake, "THB")
+    elif  x==3:
+        NoApple = int(input("Please input amount of Apple ="))
+        print("Total Price :", 80, "x", NoApple, "=", 80*NoApple, "THB")
+else:
+    print("Incorrect Username or Password")
+
