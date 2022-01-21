8b50dc6116a3bdd6c38a9c997ff7d61498e72b15
ChaydomTha <97500068+chayodomTha@users.noreply.github.com>
2022-01-21 13:47:57 +0700

    Create Lecture72_Chayodom_T.py

---
+++ Lecture72_Chayodom_T.py
@@ -0,0 +1,22 @@
+menuList = []
+
+
+def showBill():
+    totalPrice = 0
+    print("-----My Food-----")
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+        totalPrice = totalPrice + int(menuList[number][1])
+    print("Total Price :", totalPrice, "THB")
+   
+while True:
+    menuName = input("Please Enter Menu :")
+    
+    if (menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append([menuName,menuPrice])
+
+
+showBill()
